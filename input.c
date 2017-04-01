9cfb0d2136e31b119f6753c44e7cd4fd887e6c7d
LiangQiangqiang <LiangQiangqiang@pkunity.com>
2017-04-01 18:12:35 +0800

    Add char device options for mouse

--- input.c
+++ input.c
@@ -7,6 +7,7 @@
 #include <linux/interrupt.h>
 #include <linux/slab.h>
 #include <linux/list.h>
+#include <linux/cdev.h>
 
 #define  BUTTON_IRQ 0xb
 #define  KEY_MAP_SIZE 113
@@ -16,8 +17,15 @@ struct scan_code {
 	struct list_head list;
 };
 
+struct virt_mouse {
+	struct cdev	mousecdev;
+	dev_t		devno;
+	struct class * virt_mouse_class;
+};
+
 struct virt_device {
 	struct input_dev *button_dev;
+	struct virt_mouse virtmouse;
 	struct work_struct input_work;
 	struct workqueue_struct *input_workqueue;
 	struct bin_attribute bin;
@@ -144,8 +152,11 @@ static irqreturn_t button_interrupt(int irq,void *dev_instance)
 {
 	if (intr_lock) {
 		intr_lock = 0;
-		input_report_key(virt_dev.button_dev, BTN_LEFT, 1);
-		input_report_key(virt_dev.button_dev, BTN_LEFT, 0);
+		printk("interrupt\n");
+//		input_report_key(virt_dev.button_dev, BTN_LEFT, 1);
+//		input_report_key(virt_dev.button_dev, BTN_LEFT, 0);
+		input_report_rel(virt_dev.button_dev, REL_X, 10);
+		input_report_rel(virt_dev.button_dev, REL_Y, 10);
 		input_sync(virt_dev.button_dev);
 	}
 	return IRQ_RETVAL(1);
@@ -198,6 +209,94 @@ static ssize_t input_bin_read(struct file *filp, struct kobject *kobj,
 	return 0;
 }
 
+#define DEV_MAJOR_NUM 250
+#define DEV_MINOR_NUM 1
+
+int virt_mouse_open(struct inode *fnode, struct file *fp)
+{
+        return 0;
+}
+
+int virt_mouse_release(struct inode *fnod, struct file *filp)
+{
+        return 0;
+}
+ssize_t virt_mouse_read(struct file *filp, char __user *buffer, size_t size, loff_t *ppos)
+{
+        return 0;
+}
+
+ssize_t virt_mouse_write(struct file *filp, const char __user *buffer, size_t size, loff_t *ppos)
+{
+	return 0;
+}
+long virt_mouse_ioctl(struct file *filp, unsigned int cmd , unsigned long arg)
+{
+        switch(cmd) {
+	case 'a':
+		break;
+	}
+	return 0;
+}
+
+
+static const struct  file_operations virtmouse_ops = {
+        .open   = virt_mouse_open,
+        .read   = virt_mouse_read,
+        .write  = virt_mouse_write,
+        .unlocked_ioctl = virt_mouse_ioctl,
+};
+static int register_virtmouse(void)
+{
+	int ret;
+	struct device *dev = NULL;
+
+	virt_dev.virtmouse.devno = MKDEV(DEV_MAJOR_NUM, DEV_MINOR_NUM);
+	ret = register_chrdev_region(virt_dev.virtmouse.devno, 1, "virtmouse");
+	if(ret < 0) {
+		printk("%s: request_chrdev_region failed,ret=0x%x\n",__func__,ret);
+		return -1;
+	}
+	cdev_init(&(virt_dev.virtmouse.mousecdev), &virtmouse_ops);
+	virt_dev.virtmouse.mousecdev.owner = THIS_MODULE;
+	ret = cdev_add(&(virt_dev.virtmouse.mousecdev), virt_dev.virtmouse.devno, 1);
+	if (ret) {
+		printk("%s: cdev add failed ret=0x%x \n",__func__,ret);
+		goto unreg_region;
+	}
+	virt_dev.virtmouse.virt_mouse_class = class_create(THIS_MODULE,"virt_mouse_class");
+        if(IS_ERR(virt_dev.virtmouse.virt_mouse_class)) {
+                printk("create class failed \n");
+		goto unreg_cdev;
+        }
+
+	dev = device_create(virt_dev.virtmouse.virt_mouse_class, NULL,
+				virt_dev.virtmouse.devno, NULL, "virtmouse");
+        if(IS_ERR(dev)) {
+                printk("create device failed \n");
+                goto unreg_class;
+        }
+
+	return 0;
+
+unreg_class:
+	class_destroy(virt_dev.virtmouse.virt_mouse_class);
+unreg_cdev:
+	cdev_del(&(virt_dev.virtmouse.mousecdev));
+unreg_region:
+	unregister_chrdev_region(virt_dev.virtmouse.devno ,1);
+
+	return -1;
+}
+
+static void unregister_virtmouse(void)
+{
+	device_destroy(virt_dev.virtmouse.virt_mouse_class, virt_dev.virtmouse.devno);
+	class_destroy(virt_dev.virtmouse.virt_mouse_class);
+	cdev_del(&(virt_dev.virtmouse.mousecdev));
+	unregister_chrdev_region(virt_dev.virtmouse.devno, 1);
+}
+
 static int __init button_init(void)
 {
 	int error;
@@ -224,7 +323,11 @@ static int __init button_init(void)
 	set_bit(EV_REP, virt_dev.button_dev->evbit);
 	set_bit(EV_KEY, virt_dev.button_dev->evbit);
 	set_bit(EV_ABS, virt_dev.button_dev->evbit);
+	set_bit(EV_REL, virt_dev.button_dev->evbit);
 
+	/*actions for mouse*/
+	set_bit(REL_X, virt_dev.button_dev->relbit);
+	set_bit(REL_Y, virt_dev.button_dev->relbit);
 	set_bit(BTN_LEFT, virt_dev.button_dev->keybit);
 	set_bit(BTN_RIGHT, virt_dev.button_dev->keybit);
 	set_bit(BTN_MIDDLE, virt_dev.button_dev->keybit);
@@ -258,6 +361,8 @@ static int __init button_init(void)
 
 	INIT_LIST_HEAD(&(virt_dev.code_list));
 
+	register_virtmouse();
+
 	return 0;
 
 err_unreg_queue:
@@ -278,6 +383,7 @@ static void __exit button_exit(void)
 	free_irq(BUTTON_IRQ, virt_dev.button_dev);
 	if (!virt_dev.input_workqueue)
 		destroy_workqueue(virt_dev.input_workqueue);
+	unregister_virtmouse();
 }
 
 module_init(button_init);
