aed259c4e45ee745813de79dc2fde9139e23177e
LiangQiangqiang <LiangQiangqiang@pkunity.com>
2017-04-01 18:15:13 +0800

    Manager project folder

---
+++ include/common.h
@@ -0,0 +1,10 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#define VT_MOUSE_BT_LEFTT	_IOWR('H',1,int)
+#define VT_MOUSE_BT_RIGHT	_IOWR('H',2,int)
+#define VT_MOUSE_BT_MIDDL	_IOWR('H',3,int)
+
+#define VT_MOUSE_REL		_IOWR('R',1,int)
+
+#endif
